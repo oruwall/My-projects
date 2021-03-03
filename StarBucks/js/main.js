b82a404f9d43e0ecee85daca9819edb0e7d09986
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-03-03 16:03:22 +0200

    final xd

--- StarBucks/js/main.js
+++ StarBucks/js/main.js
@@ -1,8 +1,27 @@
-$(function(){
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-      });
+$('.content').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  prevArrow: '.arrow_prev',
+  nextArrow: '.arrow_next',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
