5272b8673e6b06481629ce103192575df4cdfc51
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-03-09 16:55:04 +0200

    вф

--- StarBuks2/js/main.js
+++ StarBuks2/js/main.js
@@ -1,3 +1,14 @@
+(function () {
+	const header = document.querySelector('.header')
+	window.onscroll = () => {
+		if (window.pageYOffset > 50) {
+			header.classList.add('header_active');
+		} else {
+			header.classList.remove('header_active');
+		}
+	};
+}());
+
 $('.content2').slick({
     centerMode: true,
     centerPadding: '60px',
@@ -26,3 +37,73 @@ $('.content2').slick({
     ]
   });
   
+
+    //   // Функция ymaps.ready() будет вызвана, когда
+    // // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    // ymaps.ready(init);
+    // function init(){
+    //     // Создание карты.
+    //     var myMap = new ymaps.Map("map", {
+    //         // Координаты центра карты.
+    //         // Порядок по умолчанию: «широта, долгота».
+    //         // Чтобы не определять координаты центра карты вручную,
+    //         // воспользуйтесь инструментом Определение координат.
+    //         center: [55.76, 37.64],
+    //         // Уровень масштабирования. Допустимые значения:
+    //         // от 0 (весь мир) до 19.
+    //         zoom: 7
+    //     });
+    // }
+
+    // function init() {
+    //     var myMap = new ymaps.Map('map', {
+    //         center: [55.74, 37.58],
+    //         zoom: 13,
+    //         controls: []
+    //     });
+        
+    //     // Создадим экземпляр элемента управления «поиск по карте»
+    //     // с установленной опцией провайдера данных для поиска по организациям.
+    //     var searchControl = new ymaps.control.SearchControl({
+    //         options: {
+    //             provider: 'yandex#search'
+    //         }
+    //     });
+        
+    //     myMap.controls.add(searchControl);
+        
+    //     // Программно выполним поиск определённых кафе в текущей
+    //     // прямоугольной области карты.
+    //     searchControl.search('Starbucks');
+    // }
+    
+    // ymaps.ready(init);
+
+    var location = ymaps.geolocation;
+var myMap = new ymaps.Map('map', {
+    center: [55.76, 37.64],
+    zoom: 10
+}, {
+    searchControlProvider: 'yandex#search'
+});
+
+// Получение местоположения и автоматическое отображение его на карте.
+location.get({
+        mapStateAutoApply: true
+    })
+.then(
+    function(result) {
+        // Получение местоположения пользователя.
+        var userAddress = result.geoObjects.get(0).properties.get('text');
+        var userCoodinates = result.geoObjects.get(0).geometry.getCoordinates();
+        // Пропишем полученный адрес в балуне.
+        result.geoObjects.get(0).properties.set({
+            balloonContentBody: 'Адрес: ' + userAddress +
+                                '<br/>Координаты:' + userCoodinates
+    });
+        myMap.geoObjects.add(result.geoObjects)
+    },
+    function(err) {
+        console.log('Ошибка: ' + err)
+    }
+)
