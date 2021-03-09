7f83d384b2f57dfd9d8cf62f3429d45735fd926b
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-03-09 19:32:31 +0200

    new

--- StarBuks2/js/main.js
+++ StarBuks2/js/main.js
@@ -12,6 +12,7 @@
 let scene = document.getElementById('scene');
 let parallaxInstance = new Parallax(scene);
 
+
 $('.content2').slick({
     centerMode: true,
     centerPadding: '60px',
@@ -39,32 +40,6 @@ $('.content2').slick({
       }
     ]
   });
-
-    // function init() {
-    //     var myMap = new ymaps.Map('map', {
-    //         center: [55.74, 37.58],
-    //         zoom: 13,
-    //         controls: []
-    //     });
-        
-    //     // Создадим экземпляр элемента управления «поиск по карте»
-    //     // с установленной опцией провайдера данных для поиска по организациям.
-    //     var searchControl = new ymaps.control.SearchControl({
-    //         options: {
-    //             provider: 'yandex#search'
-    //         }
-    //     });
-        
-    //     myMap.controls.add(searchControl);
-        
-    //     // Программно выполним поиск определённых кафе в текущей
-    //     // прямоугольной области карты.
-    //     searchControl.search('Starbucks');
-    // }
-    
-    // ymaps.ready(init);
-
-   
     ymaps.ready(init);
 
 function init() {
