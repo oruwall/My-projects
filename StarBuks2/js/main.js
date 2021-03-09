1df5f057ffdb48671dbdf533075ca484b946f918
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-03-09 11:28:42 +0200

    ice blended

---
+++ StarBuks2/js/main.js
@@ -0,0 +1,28 @@
+$('.content2').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    prevArrow: '.arrow_prev',
+    nextArrow: '.arrow_next',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  
