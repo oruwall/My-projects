944edc056595610b0f878a1fe743cbf155d4b9b0
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-20 21:09:42 +0300

    tabs nahui

--- Remus/js/main.js
+++ Remus/js/main.js
@@ -11,3 +11,33 @@ $(document).ready(function () {
     $("body").removeClass("lock");
   });
 });
+
+//! Tabs
+
+const tabsBtn = document.querySelectorAll(".order-describe");
+const tabsItems = document.querySelectorAll(".order-images");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener("click", function () {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+
+    if (!currentBtn.classList.contains("active")) {
+      tabsBtn.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabsItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
+
+document.querySelector(".tabs__nav-btn").click();
--- jquery-practic/js/main.js
+++ jquery-practic/js/main.js
@@ -1,77 +1,81 @@
 //! Tabs
 
-$('.card').click(function() {
-    let currTab = $(this).parent().index();
+$(".card").click(function () {
+  let currTab = $(this).parent().index();
 
-    $('.card').removeClass('card-active');
-    $(this).addClass('card-active');
+  $(".card").removeClass("card-active");
+  $(this).addClass("card-active");
 
-    $('.tab-content').removeClass('active');
-    $('.tab-content').eq(currTab).addClass('active');
+  $(".tab-content").removeClass("active");
+  $(".tab-content").eq(currTab).addClass("active");
 });
 
 //! Burger
 
-$('.hamburger').click(function() {
-    $('.head__menu').toggle();
+$(".hamburger").click(function () {
+  $(".head__menu").toggle();
 });
 
-$('.menu-close').click(function() {
-    $('.head__menu').hide();
-})
+$(".menu-close").click(function () {
+  $(".head__menu").hide();
+});
 
 //! Paralax
 
-let scene = document.getElementById('scene');
+let scene = document.getElementById("scene");
 let parallaxInstance = new Parallax(scene);
 
-//! Slider 
+//! Slider
 
-const swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    spaceBetween: 50,
-    slidesPerView: 3,
-    loop: true,
-    stopOnLastSlide: false,
-    autoplay: {
-        delay: 2000
-    }
+const swiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  spaceBetween: 50,
+  slidesPerView: 3,
+  loop: true,
+  stopOnLastSlide: false,
+  autoplay: {
+    delay: 2000,
+  },
 });
 
 //! Yandex Map
 
-    ymaps.ready(init);
-    function init(){
-        var myMap = new ymaps.Map("map", {
-            center: [55.76, 37.64],
-            zoom: 7,
-            controls: ['zoomControl', 'geolocationControl']
-        });
-    }
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 7,
+    controls: ["zoomControl", "geolocationControl"],
+  });
+}
 
 //! Validation
 
-$.validator.addMethod('regex', function(value, element, regexp){
+$.validator.addMethod(
+  "regex",
+  function (value, element, regexp) {
     let regExsp = new RegExp(regexp);
     return regExsp.text(value);
-}, 'Please check your input');
+  },
+  "Please check your input"
+);
 
-$('form').validate({
-    rules: {
-        firstName: {
-            required: true,
-            regex: "[A-Za-z]{1,32}"
-        },
-        phoneNumber: {
-            digits: true,
-            required: true,
-            minlength: 10,
-            maxlength: 11,
-            regex: "[0-9]+"
-        }
+$("form").validate({
+  rules: {
+    firstName: {
+      required: true,
+      regex: "[A-Za-z]{1,32}",
     },
-    messages: {
-        firstName: 'Введите имя правельно',
-        phoneNumber: 'Введите ваш номер'
-    }
-})
+    phoneNumber: {
+      digits: true,
+      required: true,
+      minlength: 10,
+      maxlength: 11,
+      regex: "[0-9]+",
+    },
+  },
+  messages: {
+    firstName: "Введите имя правельно",
+    phoneNumber: "Введите ваш номер",
+  },
+});
