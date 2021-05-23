1a21d5d14b41214fe1f6a00b6eb648490e2f387f
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-23 22:17:52 +0300

    gallery finish

--- Remus/js/product.js
+++ Remus/js/product.js
@@ -1,31 +1,3 @@
-// //! Tabs
-
-// const tabsBtnPro = document.querySelectorAll(".nav__link-product");
-// const tabsItemsPro = document.querySelectorAll(".shop-item");
-
-// tabsBtnPro.forEach(onTabClick);
-
-// function onTabClick(item) {
-//   item.addEventListener("click", function () {
-//     let currentBtn = item;
-//     let tabId = currentBtn.getAttribute("data-tab");
-//     let currentTab = document.querySelector(tabId);
-
-//     if (!currentBtn.classList.contains("active")) {
-//       tabsBtnPro.forEach(function (item) {
-//         item.classList.remove("active");
-//       });
-
-//       tabsItemsPro.forEach(function (item) {
-//         item.classList.remove("active");
-//       });
-
-//       currentBtn.classList.add("active");
-//       currentTab.classList.add("active");
-//     }
-//   });
-// }
-
 $(function () {
   $("ul.tabs__caption").on("click", "li:not(.active)", function (e) {
     e.preventDefault();
